14ebd1524139f4c2eb98d038d4758955cb829aa9
Vaibhav Surapaneni <vaibhavsurapaneni1979@gmail.com>
2025-01-23 16:30:41 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,11 @@ console.log(employeeName, typeof employeeName);
 console.log(employeeID, typeof employeeID);
 console.log(isActive, typeof isActive);
 
+//Task 2 - Product Details
+let productName = "Wireless mouse"; //Product's name
+const productPrice = 29.99; //Product's price
+var isAvailable = true; //Product availability
+
+console.log(productName, typeof productName);
+console.log(productPrice, typeof productPrice);
+console.log(isAvailable, typeof isAvailable);
