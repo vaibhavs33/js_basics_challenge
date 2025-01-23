014aa316966e14ec200b24a1dbad17bc8abf1dfb
Vaibhav Surapaneni <vaibhavsurapaneni1979@gmail.com>
2025-01-23 14:55:25 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+//Task 1 - Employee Information
+let employeeName = "Miguel Castro"; //Employee's name
+const employeeID = 5; //Employee's ID
+var isActive = true; //Employee's active status
+
+console.log(employeeName, typeof employeeName);
+console.log(employeeID, typeof employeeID);
+console.log(isActive, typeof isActive);
+
