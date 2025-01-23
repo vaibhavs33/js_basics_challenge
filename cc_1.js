a6c599b929dd3950c743a68454d479e87bc90261
Vaibhav Surapaneni <vaibhavsurapaneni1979@gmail.com>
2025-01-23 16:46:27 -0500

    Task 3 - Financial Transactions

--- cc_1.js
+++ cc_1.js
@@ -15,3 +15,19 @@ var isAvailable = true; //Product availability
 console.log(productName, typeof productName);
 console.log(productPrice, typeof productPrice);
 console.log(isAvailable, typeof isAvailable);
+
+//Task 3 - Financial Transactions
+let accountBalance = 350; //Initial balance
+
+//Calculations
+accountBalance += 300; //Adding 300
+console.log("Account balance after adding is:", accountBalance);
+
+accountBalance -= 150; //Subtracting 150
+console.log("Account balance after subtracting is:", accountBalance);
+
+accountBalance *= 2.5; //Multiplying by 2.5
+console.log("Account balance after multiplying is:", accountBalance);
+
+accountBalance /= 4; //Dividing by 4
+console.log("Account balance after dividing is:", accountBalance); 
